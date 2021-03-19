3d0db0bb82afffd0a16ab2c98950f768516ec35c
rrugbeer <rrugbeer@students.cs.ubc.ca>
2021-03-18 21:00:05 -0700

    map added more

--- js/main.js
+++ js/main.js
@@ -1,33 +1,26 @@
-let countyURL =
+let mapUrL =
     "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json";
-let educationURL =
-    "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json";
 
-let countyData;
-let educationData;
+let mapData;
+let crashData;
 let usMap;
 
-// Create Map
-d3.json(countyURL).then((data, error) => {
-    if (error) {
-        console.log(error);
-    } else {
-        countyData = data;
-        countyData = topojson.feature(countyData, countyData.objects.counties)
-            .features;
-        console.log("County Data");
-        console.log(countyData);
+//Parse crash data (joined table csv)
+d3.csv('data/joinTable.csv').then((_crashData) => {
+    crashData = _crashData;
+    console.log("crash data");
+    console.log(crashData);
 
-        d3.json(educationURL).then((data, error) => {
-            if (error) {
-                console.log(error);
-            } else {
-                educationData = data;
-                console.log("Education Data");
-                console.log(educationData);
-                usMap = new USMap({ parentElement: '#us-map'}, data, educationData, countyData);
-                usMap.updateVis();
-            }
-        });
-    }
-});
+    // Create Map
+    d3.json(mapUrL).then((_mapData) => {
+            mapData = _mapData;
+            mapData = topojson.feature(mapData, mapData.objects.states)
+                .features;
+            console.log("Map Data");
+            console.log(mapData);
+            usMap = new USMap({ parentElement: '#us-map'}, crashData, mapData);
+            usMap.updateVis();
+    }).catch(error => console.error(error));
+
+}).catch(error => console.error(error));
+
--- js/usMap.js
+++ js/usMap.js
@@ -7,7 +7,7 @@ class USMap {
      * @param {Array}
      *
      */
-    constructor(_config, _data, _educationData, _countyData) {
+    constructor(_config, _crashData, _statesData) {
         this.config = {
             parentElement: _config.parentElement,
             containerWidth: _config.containerWidth || 600,
@@ -16,9 +16,8 @@ class USMap {
             tooltipPadding: _config.tooltipPadding || 15
 
         };
-        this.data = _data;
-        this.countyData = _countyData;
-        this.educationData = _educationData;
+        this.crashData = _crashData;
+        this.statesData = _statesData;
         this.initVis();
     }
     initVis() {
@@ -32,67 +31,40 @@ class USMap {
     }
 
     renderVis() {
-        let vis = this;
-        let canvas = d3.select("#canvas");
-        let tooltip = d3.select("#tooltip");
+            let vis = this;
+            let usMap = d3.select("#us-map");
+            let tooltip = d3.select("#tooltip");
+            usMap
+                .selectAll("path")
+                .data(vis.statesData)
+                .enter()
+                .append("path")
+                .attr("d", d3.geoPath())
+                .attr("class", "state")
+                .attr("fill", "white")
+                .attr("stroke", "black")
+                // .attr("data-fips", (item) => {
+                //     return item["id"];
+                // })
+                // .attr("data-education", (item) => {
+                //     let fips = item["id"];
+                //     let county = vis.educationData.find((county) => {
+                //         return county["fips"] === fips;
+                //     });
+                //     let percentage = county["bachelorsOrHigher"];
+                //     return percentage;
+                // })
+                .on("mouseover", (item) => {
+                    tooltip.transition().style("visibility", "visible");
 
-        canvas
-            .selectAll("path")
-            .data(vis.countyData)
-            .enter()
-            .append("path")
-            .attr("d", d3.geoPath())
-            .attr("class", "county")
-            .attr("fill", (item) => {
-                let fips = item["id"];
-                let county = vis.educationData.find((county) => {
-                    return county["fips"] === fips;
+                    tooltip.text(
+                        "HELLO"
+                    );
+
+                    tooltip.attr("hi");
+                })
+                .on("mouseout", () => {
+                    tooltip.transition().style("visibility", "hidden");
                 });
-                let percentage = county["bachelorsOrHigher"];
-                if (percentage <= 15) {
-                    return "tomato";
-                } else if (percentage <= 30) {
-                    return "orange";
-                } else if (percentage <= 45) {
-                    return "lightgreen";
-                } else {
-                    return "limegreen";
-                }
-            })
-            .attr("data-fips", (item) => {
-                return item["id"];
-            })
-            .attr("data-education", (item) => {
-                let fips = item["id"];
-                let county = vis.educationData.find((county) => {
-                    return county["fips"] === fips;
-                });
-                let percentage = county["bachelorsOrHigher"];
-                return percentage;
-            })
-            .on("mouseover", (item) => {
-                tooltip.transition().style("visibility", "visible");
-
-                let fips = item["id"];
-                let county = vis.educationData.find((county) => {
-                    return county["fips"] === fips;
-                });
-
-                tooltip.text(
-                    county["fips"] +
-                    " - " +
-                    county["area_name"] +
-                    ", " +
-                    county["state"] +
-                    " : " +
-                    county["bachelorsOrHigher"] +
-                    "%"
-                );
-
-                tooltip.attr("data-education", county["bachelorsOrHigher"]);
-            })
-            .on("mouseout", (item) => {
-                tooltip.transition().style("visibility", "hidden");
-            });
-    }
+        }
 }
